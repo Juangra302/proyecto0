1ee3298b7bf8f4e285a7683cff725f2af70fe669
juangra <juangra30@gmail.com>
2020-04-01 23:51:30 +0200

    arreglos

--- javascript.js
+++ javascript.js
@@ -5,8 +5,10 @@ function validarUsuario() {
 	var pass = document.getElementById("contraseña").value;
 	// var user = $("#usuario").val();
 	// var pass = $("#contraseña").val();
-	if(user == "juan" && pass == "pepe"){
+	if(user == "juan" && pass == ""){
 		document.getElementById("result").innerHTML= "Correcto";
+		document.getElementById("principal").style.display = "block";
+		document.getElementsByClassName("contenedor_login").style.display = "none";
 	}
 	else{
 		document.getElementById("result").innerHTML= "Incorrecto";
